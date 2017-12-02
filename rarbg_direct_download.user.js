65c5b342616a2384767bee80b0dc7696a5e41948
godevilove <godevilove@ya.ru>
2017-12-02 17:22:43 +0300

    Handle /tv/* pages

--- rarbg_direct_download.user.js
+++ rarbg_direct_download.user.js
@@ -3,18 +3,20 @@
 // @namespace   any
 // @description Adds direct download torrent links to search pages on rarbg.to
 // @include     https://rarbg.to/torrents.php*
+// @include     https://rarbg.to/tv/*
 // @include     https://rarbgmirror.xyz/torrents.php*
-// @version     0.1 - 2017.11.26 16:40:00
+// @include     https://rarbgmirror.xyz/tv/*
+// @version     0.2 - 2017.12.02 17:20:00
 // @grant       none
 // ==/UserScript==
 
-function convertTextToLinks() {
+function linkifySizeColumn() {
   var torrents = document.querySelectorAll('tr.lista2');
 
   torrents.forEach(function(item, i, torrents) {
     var columns = item.getElementsByTagName('td');
     var fileLink = columns[1].getElementsByTagName('a')[0];
-    var torrentURL = fileLink.href.replace('://' + document.domain + '/torrent/', '://' + document.domain + '/download.php?id=') + '&f=' + fileLink.text + '-[rarbg.to].torrent';
+    var torrentURL = fileLink.pathname.replace('/torrent/', '/download.php?id=') + '&f=' + fileLink.text + '-[rarbg.to].torrent';
     var size = columns[3].textContent;
     var downloadNode = document.createElement('td');
     downloadNode.setAttribute('style', 'text-align: center;');
@@ -23,4 +25,47 @@ function convertTextToLinks() {
   });
 }
 
-convertTextToLinks();
+function addMutationObservers() {
+  var watchTargets = document.querySelectorAll('[id^="tvcontent_"]');
+  var watchOptions = { childList: true };
+  
+  watchTargets.forEach(function(watchTarget, i, watchTargets) {
+      
+    var observer = new MutationObserver(function(mutations) {
+      observer.disconnect();
+      mutations.forEach(function(mutation) {
+        linkifySizeColumn();
+      });
+      observer.observe(watchTarget, watchOptions);
+    });
+    observer.observe(watchTarget, watchOptions);
+    
+  });
+}
+
+if (location.pathname == '/torrents.php') // search page
+  linkifySizeColumn();
+
+if (location.pathname.startsWith('/tv/'))
+  addMutationObservers();
+
+
+
+
+function appendToPopulateTV() {
+  var torrents = document.querySelectorAll('a.tvshowClick[onclick^="populate_tv("]');
+  
+  torrents.forEach(function(item, i, torrents) {
+    item.setAttribute('onclick', item.getAttribute('onclick') + ' ' + 'linkifySizeColumn();');
+    // alert(item.onclick);
+  });
+}
+
+function _addEventListener() {
+  var torrents = document.querySelectorAll('a.tvshowClick[onclick^="populate_tv("]');
+  
+  torrents.forEach(function(item, i, torrents) {
+    item.addEventListener('click', linkifySizeColumn, true);
+    // alert(item.onclick);
+  });
+}
